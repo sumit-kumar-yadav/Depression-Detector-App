a14535983f22d3ea3ac9e2184674b5c3f95c40c0
Sumit <sumityadav0376@gmail.com>
2023-04-11 00:56:52 +0530

    Use controlled components

--- src/components/auth-component/Login.js
+++ src/components/auth-component/Login.js
@@ -2,18 +2,55 @@ import React, { Component } from "react";
 import "./auth.css";
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("this.state", this.state);
+  };
+
   render() {
     return (
       <form className="login-form">
         <span className="login-signup-header">Log In</span>
         <div className="field">
-          <input type="email" placeholder="Email" required />
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            onChange={this.handleEmailChange}
+            value={this.state.email}
+          />
         </div>
         <div className="field">
-          <input type="password" placeholder="Password" required />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            onChange={this.handlePasswordChange}
+            value={this.state.password}
+          />
         </div>
         <div className="field">
-          <button>Log In</button>
+          <button onClick={this.handleFormSubmit}>Log In</button>
         </div>
       </form>
     );
